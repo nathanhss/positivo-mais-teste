b2c2c75ec419b68001263ce7aff9b374144a3b25
Nathan H Silva <henriquenathan199@gmail.com>
2026-03-01 03:13:31 -0300

    refactor: index.js

--- app/src/index.js
+++ app/src/index.js
@@ -2,8 +2,8 @@ import app from "./config/express.js";
 import clientRoutes from "./routes/client.js";
 import { connectToMongoDB } from "./config/mongoose.js";
 import { loadEnvFile } from "node:process";
-import swaggerUi from "swagger-ui-express";
 import { swaggerDocument } from "./config/swagger.js";
+import swaggerUi from "swagger-ui-express";
 
 loadEnvFile("./config/.env");
 
@@ -13,5 +13,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(clientRoutes);
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
 });
