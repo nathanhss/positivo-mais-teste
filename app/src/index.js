22f5a0e50d96e72c75b819277717c61f934f90c5
Nathan H Silva <henriquenathan199@gmail.com>
2026-03-01 02:43:12 -0300

    feat: api index

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,17 @@
-console.log('Aplicação inicializada!');
+import app from "./config/express.js";
+import clientRoutes from "./routes/client.js";
+import { connectToMongoDB } from "./config/mongoose.js";
+import { loadEnvFile } from "node:process";
+import swaggerUi from "swagger-ui-express";
+import { swaggerDocument } from "./config/swagger.js";
+
+loadEnvFile("./config/.env");
+
+connectToMongoDB();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(clientRoutes);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
