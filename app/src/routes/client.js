241e392765576e39e6d7571e5c24117bc997738e
Nathan H Silva <henriquenathan199@gmail.com>
2026-03-01 03:15:57 -0300

    refactor: client route response

--- app/src/routes/client.js
+++ app/src/routes/client.js
@@ -1,13 +1,14 @@
+import { matchedData, validationResult } from "express-validator";
+
 import ClientController from "../controllers/client.js";
 import express from "express";
 import { validate } from "../middlewares/validator.js";
-import { validationResult } from "express-validator";
 
 const router = express.Router();
 
 router.get("/clients", async (req, res) => {
     const clients = await ClientController.getAll();
-    res.status(200).json(clients);
+    res.status(clients.status).json(clients);
 });
 
 router.post("/clients", validate("create"), async (req, res) => {
@@ -16,10 +17,15 @@ router.post("/clients", validate("create"), async (req, res) => {
         return res.status(400).json({ errors: errors.array() });
     }
     try {
-        const data = req.body;
-        console.log("data", data);
-        const client = await ClientController.create(data);
-        res.status(201).json(client);
+        const { name, email, document, username, password } = matchedData(req);
+        const client = await ClientController.create({
+            name,
+            email,
+            document,
+            username,
+            password,
+        });
+        res.status(client.status).json(client);
     } catch (error) {
         res.status(500).json({ message: error });
     }
@@ -33,7 +39,7 @@ router.get("/clients/:id", validate("get"), async (req, res) => {
     try {
         const id = req.params.id;
         const client = await ClientController.getById(id);
-        res.status(200).json(client);
+        res.status(client.status).json(client);
     } catch (error) {
         res.status(500).json({ message: error });
     }
@@ -48,7 +54,7 @@ router.put("/clients/:id", validate("update"), async (req, res) => {
         const id = req.params.id;
         const data = req.body;
         const client = await ClientController.update(id, data);
-        res.status(200).json(client);
+        res.status(client.status).json(client);
     } catch (error) {
         res.status(500).json({ message: error });
     }
